719098fb711668e6d29bfcd2663f970690a6ef35
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-05-30 20:58:59 +0200

    Implement add new sign to database

--- src/main/java/Controller/ServletGuestBook.java
+++ src/main/java/Controller/ServletGuestBook.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import DAO.DAOInterface;
+import DAO.DAOdatabase;
+import Model.Sign;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,17 +21,23 @@ public class ServletGuestBook extends HttpServlet {
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
             throws ServletException, IOException {
-
         LocalDate localDate = LocalDate.now();
         Date actualDate = Date.valueOf(localDate);
         String message = request.getParameter("provide-message");
         String name = request.getParameter("provide-name");
 
+        DAOInterface dao = new DAOdatabase("/home/michael/Dropbox/Codecool/Weekendowy/3.WEB/2nd_Sprint/1.Pre-Work/GuestBook/src/main/resources/database.properties");
+        Sign newSign= new Sign(name ,actualDate, message);
+
+
         try {
             request.setAttribute("message", String.valueOf(message));
             request.setAttribute("name", String.valueOf(name));
             request.setAttribute("date", String.valueOf(actualDate));
+            dao.addSign(newSign);
+
             response.setHeader("Test", "Success");
+
 
             RequestDispatcher dispatcher
                     = request.getRequestDispatcher("index.jsp");
